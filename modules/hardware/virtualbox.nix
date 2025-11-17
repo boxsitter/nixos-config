50a718ff26132978465b45877ec9a823a0290694
boxsitter <leyton.houck@gmail.com>
2025-11-17 13:18:05 -0800

    Remove X11 support for VirtualBox guest additions

--- modules/hardware/virtualbox.nix
+++ modules/hardware/virtualbox.nix
@@ -6,7 +6,6 @@
 {
   # VirtualBox guest additions
   virtualisation.virtualbox.guest.enable = true;
-  virtualisation.virtualbox.guest.x11 = true;  # Enable X11 support
   
   # Basic graphics for VM (no NVIDIA)
   hardware.graphics = {
