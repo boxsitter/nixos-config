9fc67331e4019b4f025b5d87682cb72845662fdd
boxsitter <leyton.houck@gmail.com>
2026-04-15 21:15:41 -0700

    Add credential.helper setting to Git configuration for credential storage

--- modules/home-manager/programs/git.nix
+++ modules/home-manager/programs/git.nix
@@ -15,6 +15,7 @@
         autocrlf = "input";  # Convert CRLF to LF on commit, keep LF on checkout
         eol = "lf";          # Always use LF in the working directory
       };
+      credential.helper = "store";  # Store credentials on disk (or use "cache" for temporary)
     };
   };
 }
